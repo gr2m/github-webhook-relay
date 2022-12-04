b02dde305eee33568cdff30938fb33400b686a0d
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2022-12-03 20:18:49 -0800

    refactor: rename class exported by default

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ import stop from "./api/stop.js";
 
 import VERSION from "./version.js";
 
-const GitHubWebHookSocketOctokit = Octokit.defaults({
+const GitHubWebHookRelayOctokit = Octokit.defaults({
   userAgent: `gr2m/github-webhook-relay/${VERSION}`,
 });
 
-export default class GitHubWebHookSocket {
+export default class GitHubWebHookRelay {
   /**
    * @param {import("./internal").Options} options
    */
@@ -28,7 +28,7 @@ export default class GitHubWebHookSocket {
       octokit:
         "octokit" in options
           ? options.octokit
-          : new GitHubWebHookSocketOctokit({ auth: options.createHookToken }),
+          : new GitHubWebHookRelayOctokit({ auth: options.createHookToken }),
     };
 
     this.on = state.eventEmitter.addListener.bind(state.eventEmitter);
